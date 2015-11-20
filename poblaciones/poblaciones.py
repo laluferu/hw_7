9a70325033abcc8347b385a9170e8be93a407302
LAURA LUCIA FERRER RUBIO <ll.ferrer10@compufi13.uniandes.edu.co>
2015-11-20 07:41:36 -0500

    Punto 2

---
+++ poblaciones/poblaciones.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+tray = np.genfromtxt("poblaciones.dat",delimiter=",")
+
+a = tray[:,0]
+
+b = tray[:,1]
+
+c = tray[:,2]
+
+d = tray[:,3]
+
+
+fig = plt.figure(figsize = (20,20))
+plt.subplot(2,3,1)
+plt.scatter(a,b)
+plt.xlabel(r'$\alpha$' )
+plt.ylabel(r'$\beta$' )
+
+plt.subplot(2,3,2)
+plt.scatter(a,c)
+plt.xlabel(r'$\alpha$' )
+plt.ylabel(r'$\gamma$' )
+
+plt.subplot(2,3,3)
+plt.scatter(a,d)
+plt.xlabel(r'$\alpha$' )
+plt.ylabel(r'$\delta$' )
+
+plt.subplot(2,3,4)
+plt.scatter(b,c)
+plt.xlabel(r'$\beta$' )
+plt.ylabel(r'$\gamma$' )
+
+plt.subplot(2,3,5)
+plt.scatter(b,d)
+plt.xlabel(r'$\beta$')
+plt.ylabel(r'$\delta$')
+
+plt.subplot(2,3,3)
+plt.scatter(c,d)
+plt.xlabel(r'$\gamma$' )
+plt.ylabel(r'$\delta$' )
+
+savefig("poblaciones.pdf",dpi = 400)
